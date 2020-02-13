37f485a64bc4cf763a80ab1a130238096e7fe2db
Raphael <raphaelnyangenya@gmail.com>
2020-02-13 16:43:36 +0300

    add multiplication function.

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+alert(multiply(number1, number2));
+alert(result);
